cc3fb07d615756e8f1712b3016c8df8010860c30
GeorgeHuan <george.huan1994@foxmail.com>
2022-08-01 21:08:03 +0800

    Update

--- src/InOneWeekend/sphere.h
+++ src/InOneWeekend/sphere.h
@@ -8,7 +8,6 @@ class Sphere : public hittable
 {
 public:
 	Sphere() {}
-	// Sphere(Point3 cen, double r) : center(cen), radius(r) {};
     Sphere(Point3 cen, double r, shared_ptr<material> m) : center(cen), radius(r), mat_ptr(m) {};
 
 	virtual bool hit(const Ray& r, double t_min, double t_max, hit_record& rec) const override;
@@ -19,7 +18,7 @@ public:
     shared_ptr<material> mat_ptr;
 };
 
-bool Sphere::hit(const Ray& r, double t_min, double t_max, hit_record& rec) const 
+bool Sphere::hit(const Ray& r, double t_min, double t_max, hit_record& rec) const
 {
     Vec3 oc = r.origin() - center;              // 射线起点到球体中心
 
@@ -38,7 +37,7 @@ bool Sphere::hit(const Ray& r, double t_min, double t_max, hit_record& rec) cons
     if (discriminant < 0) return false;
     auto sqrtd = sqrt(discriminant);
 
-    // Find the nearest root that lies in the acceptable range.
+    // 找到距离最近的根，并判断是否在可接受的范围内：[0.001, infinity]
     auto root = (-half_b - sqrtd) / a;
     if (root < t_min || t_max < root) 
     {
--- src/common/ray.h
+++ src/common/ray.h
@@ -12,6 +12,11 @@ public:
     Point3 origin() const { return orig; }
     Vec3 direction() const { return dir; }
 
+    /// <summary>
+    /// 交点
+    /// </summary>
+    /// <param name="t">距离</param>
+    /// <returns>交点坐标</returns>
     Point3 at(double t) const 
     {
         return orig + t * dir;
